5d73d67fbe006b4e00514a29504c71b408afeb8c
blogdodu <ducanaldodu@gmail.com>
2025-12-22 19:25:55 -0300

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,6 +1,87 @@
 const postsData = [
     // --- SEQUÊNCIA CRONOLÓGICA (DO MAIS NOVO PARA O MAIS ANTIGO) ---
     
+    // d002. verdades absolutas
+        {
+            id: "apenas-verdades-absolutas-ed1",
+            title: "apenas verdades absolutas - ed1",
+            date: "10 mar 2025",
+            readingTime: "3 min de leitura",
+            category: "ensaios & provocações",
+            categoryId: "#caminhos",
+            author: "du",
+            authorId: "#autores",
+            image: "img/post-verdades.png",
+            imageAlt: "imagem abstrata representando um labirinto",
+            imageCaption: "... presente...",
+            content: `
+        	    <p><center>"não existem verdades absolutas”</center></p>
+            <br>
+            <hr class="divisor-fino-longo">
+                <p><strong>♦ o paradoxo ♦</strong></p>
+            <br>
+                <p>essa afirmação, tão proclamada em debates filosóficos e conversas cotidianas, muitas vezes serve como ponto de partida para discussões sobre a natureza da verdade e da certeza....</p>
+                <p>discordo totalmente de tal afirmação, que já em si, paradoxalmente, é uma inverdade..…</p>
+                <p>ao refletirmos mais profundamente sobre essa questão, podemos chegar a uma conclusão surpreendente: só existem verdades absolutas, todas verdades são assim, absolutas, e, portanto, não há espaço na existência 	para meias-verdades ou verdades parciais...</p>
+            <br>
+            <hr class="divisor-fino-longo">
+        	    <p><strong>♦ o que define uma verdade??? ♦</strong></p>
+            <br>
+        	    <p>entenda, verdades são aquelas coisas que simplesmente acreditamos que são e pronto.... isso porque se não acreditamos 100% em uma verdade, qualquer mísera percentagem de incerteza que exista, já é o suficiente 	para não haver mais convicção nessa ou naquela verdade..... que agora já passou a ser uma dúvida, ou quem 	sabe, no máximo, uma quase verdade, que na verdade não é uma verdade....</p>
+            	<p>mas fique calma(o), as verdades, mesmo todas sendo absolutas, não são, em si, eternas e universais, mesmo as que de fato sejam verdades universais....</p>
+            	<p>explico, as verdades são pertencentes a cada pessoa, são escravas de nossos conhecimentos e experiências e do nosso arcabouço de verdades.... e assim sendo, só nós somos suas(eus) senhoras(es), suas(eus) 	deusas(es) criadoras(es) e as(os) únicas(os) com poder para dotá-las e destituí-las do título de 	verdade…</p>
+            	<p>o que realmente faz delas verdades é a capacidade de resistirem, de serem absolutas, quando confrontadas por outras que queiram ser verdades no seu lugar.....</p>
+        	    <p>portanto, nenhuma verdade criada por nós, simplesmente acaba e deixa um buraco em seu lugar, uma fissura ou um abismo de dúvidas... elas só são destronadas e substituídas por outras verdades mais fortes, maiores 	e mais verdadeiras, porém igualmente absolutas quanto suas antecessoras, um dia, assim, foram...</p>
+            <br>
+            <hr class="divisor-fino-longo">
+        	    <p><strong>♦ a dança das verdades: ciência e sociedade ♦</strong></p>
+            <br>
+        	    <p>pra exemplificar essa dinâmica da construção de verdades, podemos considerar o cenário da evolução científica ao longo da história.... no passado, muitas verdades absolutas eram aceitas sem questionamento, 	como a idéia de que a terra era plana ou o geocentrismo, que colocava a terra como o centro do 	universo....</p>
+        	    <p>no entanto, com o avanço da ciência e a busca e a acumulação de novos conhecimentos e evidências, essas verdades foram sendo substituídas por outras mais fundamentadas e abrangentes....</p>
+        	    <p>além disso, é importante dizer que a construção de novas verdades não ocorre de forma isolada, mas sim em um contexto social e cultural específico..... onde outras realidades, outras pessoas, nos confrontam com 	uma possível verdade melhor que a que carregamos e protegemos...</p>
+        	    <p>é preciso coragem de colocar nossas verdades à prova, conhecendo a fundo cada outras verdades que as desafiem.....</p>
+        	    <p>essas outras, até então, para nós, apenas possíveis verdades, também foram construídas em crenças e valores de uma outra pessoa e da sociedade que a influencia, afetando diretamente na forma de sua concepção 	e aceitação, e são tão absolutas para ela quanto as nossas são para nós…</p>
+            	<p>por isso é que é impossível convencer alguém de que o que ela(e) acredita não seja verdade..... por que na realidade do outro é....</p>
+            <br>
+            <hr class="divisor-fino-longo">
+        	    <p><strong>♦ a (des)construção interna da verdade ♦</strong></p>
+            <br>
+            	<p>você não possui os mesmos conhecimentos, as mesmas vivências, experiências que permitiram àquela verdade de outrem ser construída em si...</p>
+            	<p>cada pessoa precisa conseguir construir, por si só, consigo mesma(o), uma verdade maior que a que possui, para que ela possa ocupar o espaço daquela que já o preenche até então...</p>
+        	    <p>este processo leva tempo, requer a reunião de novos materiais e, principalmente, depende da vontade de se munir, a cada dia, de melhores verdades e de, ao se deparar com uma, conseguir enxergá-la e aceitar 	abandonar a que antes carregava e agora só lhe atrapalha....</p>
+            	<p>e é por isso que diferentes culturas podem ter sistemas de crenças e verdades distintos......</p>
+            <br>
+            <hr class="divisor-fino-longo">
+        	    <p><strong>♦ convite à reflexão e ao diálogo ♦</strong></p>
+            <br>
+            	<p>dessa forma, a manutenção, o abandono e a busca pelas verdades absolutas que estão sob nosso domínio, é uma jornada complexa e multifacetada, que envolve não apenas a racionalidade individual, mas também as 	dinâmicas coletivas de uma sociedade e de uma realidade em constante transformação....</p>
+                <p>ao reconhecermos a relatividade e mutabilidade das verdades, abrimos espaço para o diálogo, a reflexão e o aprendizado constante, fundamentais para o progresso e a evolução....</p>
+        	    <p>mas só existem verdades absolutas, tudo mais são apenas dúvidas...</p>
+            <br>
+            <br>
+            <br>
+                <div class="pos-scriptum">
+            	    <p><strong>ps.:</strong> ou não...</p>
+        	        <p>...será????</p>
+                 </div>
+            <br>
+            <hr class="divisor-fino-longo">
+            <br>
+                <p>gostou do que leu???</p>
+                <p>compartilhe suas verdades conosco.....</p>
+                <p>sua opinião é importante....</p>
+            <br>
+                <p><strong><center>♦ ensaios novos às segundas ♦</center></strong></p>
+                    <p><center>__________________________________</center></p>
+                <p><center>não prometo respostas.....</center></p>
+                <p><center>mas te convido pra conversas interessantes......</center></p>
+                    <p><center>__________________________________</center></p>
+            <br>
+            <br>
+                <p><strong><center>♦ vamos construir um espaço rico em troca de idéias e reflexões ♦</center></strong></p>
+            `
+    },
+    
     // d001. o recomeço
     {
         id: "isso-e-so-o-re-comeco-cd1",
@@ -46,4 +127,5 @@ const postsData = [
     },
 
 ];
+
 
