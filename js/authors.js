5541fff839fbff59927228c0ff91918736da12c1
blogdodu <ducanaldodu@gmail.com>
2026-05-06 11:40:19 -0300

    Update authors.js

--- js/authors.js
+++ js/authors.js
@@ -5,6 +5,7 @@ id: "du",
 nome: "du",
 papel: "criador e curador",
 foto: "img/du-perfil.jpg",
+legenda_foto: "eu, você, nós e além!!!",
 bio: `
 <p class="destaque">oi.... eu sou o du....</p><br>
 <p>e esse é o <strong>blog do du</strong>, um espaço onde junto tudo que sou e o que me tange......</p><br>
@@ -18,6 +19,17 @@ bio: `
 <p>mas te convido pra conversas interessantes......</p>
 <hr class="divisor-fino-longo"><br>
 <p class="assinatura">vem junto, que a gente se encontra nas palavras....</p>
+</div>
+`,
+apoio: `
+<div id="apoio-template" class="apoio-container">
+<p>considere apoiar o projeto <strong>a partir de R$ 5,oo</strong></p>
+<p><a href="https://apoia.se/blogdodu"><strong>https://apoia.se/blogdodu</strong></a></p>
+<div class="pix-info">
+<p>chave pix para apoiar diretamente:</p>
+<p><strong>pix.ducanal@gmail.com</strong></p>
+<p class="detalhe">(sim, é um email feio e sem glamour - mas o café que ele proporciona é real e aquece o coração....)</p>
+</div>
 </div>
 `,
 links: { instagram: "@eduardobuback", email: "pix.ducanal@gmail.com" }
