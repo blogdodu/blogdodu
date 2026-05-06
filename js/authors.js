3fd9a0e14ff36e4b521623ef7f2ef8f570c83834
blogdodu <ducanaldodu@gmail.com>
2026-05-06 11:14:51 -0300

    Add files via upload

---
+++ js/authors.js
@@ -0,0 +1,26 @@
+const authorsData = [
+
+{
+id: "du",
+nome: "du",
+papel: "criador e curador",
+foto: "img/du-perfil.jpg",
+bio: `
+<p class="destaque">oi.... eu sou o du....</p><br>
+<p>e esse é o <strong>blog do du</strong>, um espaço onde junto tudo que sou e o que me tange......</p><br>
+<p>acho que não sei existir de um jeito só: sou designer, fotógrafo, músico, compositor, iluminador cênico, cenógrafo, produtor artístico cultural, massoterapeuta e 'otras cositas más'... rsrsrs..... ah! e claro, nas horas mais intensas, <strong>escritor</strong>....</p><br>
+<p>desde 2019 venho enfrentando uma depressão e escrever tem sido uma forma de respirar..... cada texto é um passo..... cada palavra, uma tentativa de voltar a sentir.....</p><br>
+<p>e disso nasce esse blog, de uma <strong>vontade de compartilhar, de criar conexões</strong> em tempos de tanta distração.....</p><br>
+<p>uma proposta de <strong>slow content</strong>..</p><br>
+<hr class="divisor-fino-longo">
+<div class="chamada-final">
+<p>não prometo respostas.....</p>
+<p>mas te convido pra conversas interessantes......</p>
+<hr class="divisor-fino-longo"><br>
+<p class="assinatura">vem junto, que a gente se encontra nas palavras....</p>
+</div>
+`,
+links: { instagram: "@eduardobuback", email: "pix.ducanal@gmail.com" }
+}
+
+];
