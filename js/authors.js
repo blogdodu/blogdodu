69d87edc70d8c7edd8ec428c391e3e26e057620f
blogdodu <ducanaldodu@gmail.com>
2026-05-12 14:24:16 -0300

    Update authors.js

--- js/authors.js
+++ js/authors.js
@@ -1,38 +1,55 @@
-const authorsData = [
+const authorsData = 
+[
 
-{
-id: "du",
-nome: "du",
-papel: "criador e curador",
-foto: "img/du-perfil.jpg",
-legenda_foto: "eu, você, nós e além!!!",
-bio: `
-<p class="destaque">oi.... eu sou o du....</p><br>
-<p>e esse é o <strong>blog do du</strong>, um espaço onde junto tudo que sou e o que me tange......</p><br>
-<p>acho que não sei existir de um jeito só: sou designer, fotógrafo, músico, compositor, iluminador cênico, cenógrafo, produtor artístico cultural, massoterapeuta e 'otras cositas más'... rsrsrs..... ah! e claro, nas horas mais intensas, <strong>escritor</strong>....</p><br>
-<p>desde 2019 venho enfrentando uma depressão e escrever tem sido uma forma de respirar..... cada texto é um passo..... cada palavra, uma tentativa de voltar a sentir.....</p><br>
-<p>e disso nasce esse blog, de uma <strong>vontade de compartilhar, de criar conexões</strong> em tempos de tanta distração.....</p><br>
-<p>uma proposta de <strong>slow content</strong>..</p><br>
-<hr class="divisor-fino-longo">
-<div class="chamada-final">
-<p>não prometo respostas.....</p>
-<p>mas te convido pra conversas interessantes......</p>
-<hr class="divisor-fino-longo"><br>
-<p class="assinatura">vem junto, que a gente se encontra nas palavras....</p>
-</div>
-`,
-apoio: `
-<div id="apoio-template" class="apoio-container">
-<p>considere apoiar o projeto <strong>a partir de R$ 5,oo</strong></p>
-<p><a href="https://apoia.se/blogdodu"><strong>https://apoia.se/blogdodu</strong></a></p>
-<div class="pix-info">
-<p>chave pix para apoiar diretamente:</p>
-<p><strong>pix.ducanal@gmail.com</strong></p>
-<p class="detalhe">(sim, é um email feio e sem glamour - mas o café que ele proporciona é real e aquece o coração....)</p>
-</div>
-</div>
-`,
-links: { instagram: "@eduardobuback", email: "pix.ducanal@gmail.com" }
-}
+	// du, autor principal
+	{
+		id: "du",
+		nome: "du",
+		papel: "criador e curador",
+		foto: "/img/du-perfil.jpg",
+		legenda_foto: "eu, você, nós e além!!!",
+		bio: 
+		`
+				<p class="destaque">oi.... eu sou o du....</p>
+			<br>
+				<p>e esse é o <strong>blog do du</strong>, um espaço onde junto tudo que sou e o que me tange......</p>
+			<br>
+				<p>acho que não sei existir de um jeito só: sou designer, fotógrafo, músico, compositor, iluminador cênico, cenógrafo, produtor artístico cultural, massoterapeuta e 'otras cositas más'... rsrsrs..... ah! e claro, nas horas mais intensas, <strong>escritor</strong>....</p>
+			<br>
+				<p>desde 2019 venho enfrentando uma depressão e escrever tem sido uma forma de respirar..... cada texto é um passo..... cada palavra, uma tentativa de voltar a sentir.....</p>
+			<br>
+				<p>e disso nasce esse blog, de uma <strong>vontade de compartilhar, de criar conexões</strong> em tempos de tanta distração.....</p>
+			<br>
+				<p>uma proposta de <strong>slow content</strong>..</p>
+			<br>
+			<hr class="divisor-fino-longo">
+			<div class="chamada-final">
+					<p>não prometo respostas.....</p>
+					<p>mas te convido pra conversas interessantes......</p>
+			<hr class="divisor-fino-longo">
+				<br>
+					<p class="assinatura">vem junto, que a gente se encontra nas palavras....</p>
+			</div>
+		`,
+		apoio: 
+		`
+			<div id="apoio-template" class="apoio-container">
+					<p>considere apoiar o projeto <strong>a partir de R$ 5,oo</strong></p>
+					<p><a href="https://apoia.se/blogdodu"><strong>https://apoia.se/blogdodu</strong></a></p>
+				<br>
+				<div class="pix-info">
+					<p>chave pix para apoiar diretamente:</p>
+					<p><strong>pix.ducanal@gmail.com</strong></p>
+					<p class="detalhe">(sim, é um email feio e sem glamour - mas o café que ele proporciona é real e aquece o coração....)</p>
+				</div>
+			</div>
+		`,
+		links:
+		{
+			instagram: "@eduardobuback",
+			instagramLink: "https://www.instagram.com/eduardobuback",
+			email: "pix.ducanal@gmail.com"
+		}
+	},
 
 ];
