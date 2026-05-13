250153ace9975a70ea76e3c477248f35cbbaa0dc
blogdodu <ducanaldodu@gmail.com>
2026-05-12 21:15:43 -0300

    Update authors.js

--- js/authors.js
+++ js/authors.js
@@ -52,4 +52,50 @@ const authorsData =
 		}
 	},
 
+
+		// du, autor principal
+	{
+		id: "jefferson-araujo",
+		nome: "jefferson araújo",
+		papel: "poeta",
+		foto: "/img/jefferson-araujo-perfil.png",
+		legenda_foto: " ",
+		bio: 
+		`
+				<p>Nascido em Vitória/ES no ano de 1983, é pós-graduado em administração, escritor, romancista, poeta, sonetista, contista, compositor musical, cantor e violonista.</p>
+			<br>
+				<p>É membro da Academia Maçônica de Letras do Espírito Santo (cadeira 03) e um dos fundadores da Academia Cariaciquense de Letras (cadeira 09), atualmente presidente da 5ª administração.</p>
+			<br>
+				<p>Iniciou-se na Maçonaria em 2017 na A∴ R∴ L∴ S∴ IIr∴ Alcebíades d'Ávila e Jayme Bulhões n º 2931, oriente de Vila Velha/ES.</p>
+			<br>
+				<p>Também é um dos fundadores da A∴ R∴ L∴ S∴ Amor e Concórdia nº 4893 em 2025, oriente de Cariacica/ES atuando como secretário.</p>
+			<br>
+				<p>É autor dos livros: “Pétalas e Espinhos: Do Soneto à Catarse - As Quatro Emoções à Flor da Pele” e “A Viúva Negra: O Livro da Lua - Prelúdio”, dentre outros projetos literários, atuando como coautor, organizador e curador.</p>
+			<br>
+				<p>No âmbito musical, destaca-se como líder das bandas “Banda João Bananeira” e “Bossa-Congo”, além de composições e parcerias diversas, como a criação do Hino da Academia Maçônica de Letras do Espírito Santo.</p>
+			<br>
+				<p>∴</p>
+			<br>
+		`,
+		apoio: 
+		`
+			<div id="apoio-template" class="apoio-container">
+					<p>considere apoiar o projeto <strong>a partir de R$ 5,oo</strong></p>
+					<p><a href="https://apoia.se/blogdodu"><strong>https://apoia.se/blogdodu</strong></a></p>
+				<br>
+				<div class="pix-info">
+					<p>chave pix para apoiar diretamente:</p>
+					<p><strong>pix.ducanal@gmail.com</strong></p>
+					<p class="detalhe">(sim, é um email feio e sem glamour - mas o café que ele proporciona é real e aquece o coração....)</p>
+				</div>
+			</div>
+		`,
+		links:
+		{
+			instagram: "@eduardobuback",
+			instagramLink: "https://www.instagram.com/eduardobuback",
+			email: "pix.ducanal@gmail.com"
+		}
+	},
+
 ];
